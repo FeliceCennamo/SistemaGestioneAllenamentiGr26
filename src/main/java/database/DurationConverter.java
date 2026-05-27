dd38769e3d18246162f91dacbabcc09ad858d73e
FeliceCennamo <felicennamo1@gmail.com>
2026-05-27 12:02:06 +0200

    Fix dei commenti sul package database Aggiunta metodo alternativo OttieniTutti

--- src/main/java/database/DurationConverter.java
+++ src/main/java/database/DurationConverter.java
@@ -8,11 +8,22 @@ import java.time.Duration;
 // Potrebbe essere un pattern Adapter
 @Converter(autoApply = true)
 public class DurationConverter implements AttributeConverter<Duration, String> {
+
+    /**
+     * Converte un oggetto Duration in un oggetto String
+     * @param duration oggetto Duration da voler convertire
+     * @return oggetto String risultato della conversione
+     * */
     @Override
     public String convertToDatabaseColumn(Duration duration) {
         return duration == null ? null : duration.toString();
     }
 
+    /**
+     * Converte un oggetto String in un oggetto Duration
+     * @param dbData oggetto String da voler convertire
+     * @return oggetto Duration risultato della conversione
+     * */
     @Override
     public Duration convertToEntityAttribute(String dbData) {
         return dbData == null ? null : Duration.parse(dbData);
